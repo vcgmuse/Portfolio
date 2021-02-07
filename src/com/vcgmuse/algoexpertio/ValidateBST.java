a9c3416c1f4e9ea07640a01cabdc62108825a0ed
Emmanuel Vega <vcgmuse@gmail.com>
2021-02-07 16:24:03 -0600

    This is the base of my Portfolio

---
+++ src/com/vcgmuse/algoexpertio/ValidateBST.java
@@ -0,0 +1,30 @@
+package com.vcgmuse.algoexpertio;
+
+public class ValidateBST {
+  public static boolean validateBst(BST tree) {
+    // Write your code here.
+    return validateBst(tree, Integer.MIN_VALUE, Integer.MAX_VALUE);
+  }
+  public static boolean validateBst(BST tree, int minValue, int maxValue){
+    if (tree.value < minValue || tree.value >= maxValue){
+      return false;
+    }
+    if (tree.left !=null && !validateBst(tree.left, minValue, tree.value)){
+      return false;
+    }
+    if (tree.right != null && !validateBst(tree.right, tree.value, maxValue)){
+      return false;
+    }
+    return true;
+  }
+
+  static class BST {
+    public int value;
+    public BST left;
+    public BST right;
+
+    public BST(int value) {
+      this.value = value;
+    }
+  }
+}
